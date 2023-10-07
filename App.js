219cf1e9131766919456c10cf65bd42ff3471f8d
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-07 20:29:24 +0300

    🎨 style(App.js): add background image and gradient to improve visual appeal 📦 chore(package.json): add expo-linear-gradient as a dependency to enable the use of linear gradients 🎨 style(StartGameScreen.js): change background color to improve visual consistency

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground
+        source={require('./assets/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={{ opacity: 0.2 }}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1
+  }
+});
