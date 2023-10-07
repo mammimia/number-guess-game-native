a864f2f484f0920daa5ea13bcca681449e17a3b3
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-07 21:32:14 +0300

    ✨ feat(NumberContainer.js): add NumberContainer component to display a number with a styled container 🎨 style(NumberContainer.js): style the NumberContainer component with a border, padding, margin, and text styling

---
+++ components/game/NumberContainer.js
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet } from 'react-native';
+import Colors from '../../contants/colors';
+
+function NumberContainer({ children }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.number}>{children}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 4,
+    borderColor: Colors.accent500,
+    padding: 24,
+    margin: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  number: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: Colors.accent500
+  }
+});
+
+export default NumberContainer;
