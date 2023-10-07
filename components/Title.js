ba02d8700b6e7dd787bf756841fd5d30db6aa570
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-07 21:00:04 +0300

    🚀 feat(App.js): add SafeAreaView component to ensure content is displayed within safe area boundaries on different devices 🆕 feat(Title.js): create Title component to display a styled title with custom styles 🎨 style(GameScreen.js): add container styles to GameScreen component for better layout and spacing

---
+++ components/Title.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function Title({ children }) {
+  return (
+    <View>
+      <Text style={styles.title}>{children}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#ddb52f',
+    padding: 12
+  }
+});
+
+export default Title;
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Title from '../components/Title';
 
 function GameScreen() {
   return (
-    <View>
-      <Text>GameScreen</Text>
+    <View style={styles.container}>
+      <Title>Opponent's Guess</Title>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 12
+  }
+});
 
 export default GameScreen;
