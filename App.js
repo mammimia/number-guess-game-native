7b711c9a4aac0a48cf89f274227c71235308d3ab
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-08 18:53:38 +0300

    🔧 chore(App.js): optimize app startup by using expo-splash-screen to prevent auto hide and manually hide the splash screen after fonts are loaded 🔧 chore(App.js): update import path for background image in ImageBackground component to match new file location 🔧 chore(package.json): add expo-linear-gradient and expo-splash-screen dependencies

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
+import { useFonts } from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useState } from 'react';
-import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { ImageBackground, SafeAreaView, StyleSheet, View } from 'react-native';
 import Colors from './contants/colors';
 import GameOverScreen from './screens/GameOverScreen';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [isGameOver, setIsGameOver] = useState(false);
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   const pickedNumberHandler = (number) => {
     setUserNumber(number);
@@ -31,19 +55,21 @@ export default function App() {
   }
 
   return (
-    <LinearGradient
-      colors={[Colors.primary700, Colors.accent500]}
-      style={styles.rootScreen}
-    >
-      <ImageBackground
-        source={require('./assets/background.png')}
-        resizeMode="cover"
+    <View style={styles.rootScreen} onLayout={onLayoutRootView}>
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
         style={styles.rootScreen}
-        imageStyle={{ opacity: 0.2 }}
       >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+        <ImageBackground
+          source={require('./assets/images/background.png')}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={{ opacity: 0.2 }}
+        >
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </View>
   );
 }
 
